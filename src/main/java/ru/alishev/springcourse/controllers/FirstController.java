695e74e0d991ab4f5d487db6a62a8de8921fe0af
kunjut <v.dimnich@gmail.com>
2021-03-07 21:49:46 +0300

    Use model

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,11 +1,10 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
@@ -13,14 +12,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+//      Упаковка параметров в модель
+        model.addAttribute("message", name + " " + surname);
 
         return "first/hello";
-    }
-
-    @GetMapping("/goodbye")
-    public String goodByePage() {
-        return "first/goodbye";
     }
 }
